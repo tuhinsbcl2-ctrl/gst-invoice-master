707307efd20eb3ef3a865a0e7e1dc42df7d86979
tuhinsbcl2-ctrl <tuhin.sbcl2@gmail.com>
2026-03-27 23:43:26 +0530

    Create Expense model

---
+++ models/Expense.js
@@ -0,0 +1,8 @@
+class Expense {
+    constructor(amount, description, date) {
+        this.amount = amount;
+        this.description = description;
+        this.date = date;
+    }
+}
+module.exports = Expense;
